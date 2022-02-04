ba0bf2c0e17be8c406861fd61aef038771261b1d
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-02-04 14:19:22 +0000

    added colum hover repsonsivniss

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function webchange(pTarg) {
     location.href = "pages/" + pTarg + "/" + pTarg + ".html";
-    console.log("pages/" + pTarg + "/" + pTarg + ".html");
+    //console.log("pages/" + pTarg + "/" + pTarg + ".html");
 }
