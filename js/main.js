36c0381b3b666b9990c3ed5601ce304aee85d03e
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-02-04 16:25:58 +0000

    added science landing page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
-function webchange(pTarg) {
-    location.href = "pages/" + pTarg + "/" + pTarg + ".html";
-    //console.log("pages/" + pTarg + "/" + pTarg + ".html");
+function webchange(pTarg, depth) {
+    //location.href = "pages/" + pTarg + "/" + pTarg + +"/" + pTarg + "/" + ".html";
+    //console.log("pages/" + pTarg + "/" + pTarg + "/" + pTarg + "/" + ".html");
+    console.log(pTarg);
+
+    if (depth = 1) {
+        location.href = "./pages/" + pTarg + "/" + pTarg + ".html";
+        console.log("1");
+    } else if (depth = 2) {
+        location.href = "./pages/" + pTarg + "/" + pTarg + "/" + pTarg + ".html";
+        console.log("2");
+    } else if (depth = 3) {
+        location.href = "./pages/" + pTarg + "/" + pTarg + "/" + pTarg + "/" + pTarg + ".html";
+        console.log("3");
+    }
 }
